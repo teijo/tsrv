5211269159d04d332add2badeb4f468a1680bcf6
Teijo Laine <teijo@aropupu.fi>
2013-10-20 00:20:14 +0300

    Initial Unfiltered project from giter8 template

---
+++ build.sbt
@@ -0,0 +1,18 @@
+organization := "com.example"
+
+name := "tsrv"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies ++= Seq(
+  "net.databinder" %% "unfiltered-directives" % "0.7.0",
+  "net.databinder" %% "unfiltered-filter" % "0.7.0",
+  "net.databinder" %% "unfiltered-jetty" % "0.7.0",
+  "net.databinder" %% "unfiltered-spec" % "0.7.0" % "test"
+)
+
+resolvers ++= Seq(
+  "java m2" at "http://download.java.net/maven/2"
+)
---
+++ src/main/scala/Example.scala
@@ -0,0 +1,78 @@
+package com.example
+
+import unfiltered.request._
+import unfiltered.response._
+
+import unfiltered.directives._, Directives._
+
+//** unfiltered plan */
+class App extends unfiltered.filter.Plan {
+
+  def intent = Directive.Intent {
+    case GET(r) =>
+      success(Ok ~> view(Map.empty)(<p> What say you? </p>))
+
+    case POST(Params(params)) =>
+      case class BadParam(msg: String) extends ResponseJoiner(msg)( messages =>
+          view(params)(<ul>{
+            for (message <- messages)
+            yield <li>{message}</li>
+          }</ul>)
+      )
+      implicit def required[T] = data.Requiring[T].fail(name =>
+        BadParam(name + " is missing")
+      )
+      val inputString = data.as.String ~>
+        data.as.String.trimmed ~>
+        data.as.String.nonEmpty.fail(
+          (key, _) => BadParam(s"$key is empty")
+        )
+      implicit val asInt = inputString ~> data.as.Int.fail(
+        (_, i) => BadParam(s"'$i' is not an int")
+      )
+
+      for {
+        int & word <-
+          (data.as.Required[Int] named "int") &
+          (inputString ~> data.Conditional(palindrome).fail(
+            (_, value) => BadParam(s"'$value' is not a palindrome")
+          ) ~> required named "palindrome")
+      } yield {
+        view(params)(<p>Yup. { int } is an integer and { word } is a palindrome. </p>)
+      }
+  }
+  def palindrome(s: String) = s.toLowerCase.reverse == s.toLowerCase
+  def view(params: Map[String, Seq[String]])(body: scala.xml.NodeSeq) = {
+    def p(k: String) = params.get(k).flatMap { _.headOption } getOrElse("")
+    Html5(
+     <html>
+      <head>
+        <title>uf example</title>
+        <link rel="stylesheet" type="text/css" href="/assets/css/app.css"/>
+      </head>
+      <body>
+       <div id="container">
+       { body }
+       <form method="POST">
+         <div>Integer <input type="text" name="int" value={ p("int") } /></div>
+         <div>Palindrome <input type="text" name="palindrome" value={ p("palindrome") } /></div>
+         <input type="submit" />
+       </form>
+       </div>
+     </body>
+    </html>
+   )
+  }
+}
+
+/** embedded server */
+object Server {
+  def main(args: Array[String]) {
+    unfiltered.jetty.Http.anylocal.context("/assets") {
+      _.resources(new java.net.URL(getClass().getResource("/www/css"), "."))
+    }.filter(new App).run({ svr =>
+      unfiltered.util.Browser.open(svr.url)
+    }, { svr =>
+    })
+  }
+}
