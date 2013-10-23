53b9c9b8d953c89dcdcde6dcbd80ecda948c6e49
Teijo Laine <teijo@aropupu.fi>
2013-10-24 00:03:26 +0300

    Fix template test

--- src/test/scala/ExampleSpec.scala
+++ src/test/scala/ExampleSpec.scala
@@ -2,14 +2,12 @@ package com.example
 
 import org.specs._
 
-import dispatch._
+import dispatch.classic.Http
 
 object ExampleSpec extends Specification with unfiltered.spec.jetty.Served {
-  
-  import dispatch._
-  
+
   def setup = { _.filter(new App) }
-  
+
   val http = new Http
   
   "The example app" should {
