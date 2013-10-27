d1c33351eb8f868cebbcc3b8676db29e79bb80a2
Teijo Laine <teijo@aropupu.fi>
2013-10-27 14:21:25 +0200

    Assert using JSON deserialization

--- src/test/scala/ServerSpec.scala
+++ src/test/scala/ServerSpec.scala
@@ -2,9 +2,13 @@ package tsrv
 
 import org.specs._
 
+import org.json4s._
+import org.json4s.native.Serialization.read
 import scalaj.http.{HttpOptions, Http}
 
 object ServerSpec extends Specification with unfiltered.spec.jetty.Served {
+  implicit val formats = native.Serialization.formats(NoTypeHints)
+
   def setup = { _.context("/js") {
     _.resources(new java.net.URL(getClass().getResource("/www/js/t.js"), "."))
   }.filter(new App) }
@@ -12,17 +16,20 @@ object ServerSpec extends Specification with unfiltered.spec.jetty.Served {
   "Client interface" should {
     "return a record" in {
       val result = Http.get(s"${server.url}record").asString
-      result must_== """readResponse({"id":123,"teams":["A","B"]})"""
+      val expected = Tournament(id = 123, teams = List("A", "B"))
+      read[Tournament](result) must_== expected
     }
 
     "store a record" in {
       val result = Http.post(s"${server.url}record").asString
-      result must_== """{"id":123}"""
+      val expected = CreateResponse(123)
+      read[CreateResponse](result) must_== expected
     }
 
     "update a record" in {
       val result = Http(s"${server.url}record").option(HttpOptions.method("PUT")).asString
-      result must_== """{"id":123}"""
+      val expected = CreateResponse(123)
+      read[CreateResponse](result) must_== expected
     }
   }
 
