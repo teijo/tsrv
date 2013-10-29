409d36c5771578bf2dcb41121a64577a97d92cab
Teijo Laine <teijo@aropupu.fi>
2013-10-29 23:43:25 +0200

    Parse and echo basic group state message

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -4,8 +4,15 @@ import unfiltered.request._
 import unfiltered.response._
 import org.json4s._
 import org.json4s.native.Serialization.write
+import org.json4s.native.Serialization.read
 
 import unfiltered.directives._, Directives._
+
+
+case class GroupTeam(id: Integer, name: String)
+case class GroupMatchTeam(team: Integer, score: Integer)
+case class GroupMatch(round: Integer, a: GroupMatchTeam, b: GroupMatchTeam)
+case class GroupRequest(teams: List[GroupTeam], matches: List[GroupMatch])
 
 case class CreateResponse(id: Integer)
 case class Tournament(id: Integer, teams: List[String])
@@ -15,19 +22,24 @@ class App extends unfiltered.filter.Plan {
 
   def intent = Directive.Intent {
     case OPTIONS(Path("/record")) =>
-      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~> ResponseHeader("Access-Control-Allow-Methods", Set("GET", "POST", "PUT", "OPTIONS")))
+      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~>
+        ResponseHeader("Access-Control-Allow-Methods", Set("GET", "POST", "PUT", "OPTIONS")))
 
     case GET(Path("/")) =>
       success(Ok ~> ResponseString("Hello"))
 
     case GET(Path("/record")) =>
-      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~> ResponseString(write(Tournament(id = 123, teams = List("A", "B")))))
+      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~>
+        ResponseString(write(Tournament(id = 123, teams = List("A", "B")))))
 
     case POST(Path("/record")) =>
-      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~> ResponseString(write(CreateResponse(123))))
+      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~>
+        ResponseString(write(CreateResponse(123))))
 
-    case PUT(Path("/record")) =>
-      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~> ResponseString(write(CreateResponse(123))))
+    case req @ PUT(Path("/record")) =>
+      val groupRecord = read[GroupRequest](Body.string(req))
+      success(Ok ~> ResponseHeader("Access-Control-Allow-Origin", Set("*")) ~>
+        ResponseString(write(groupRecord)))
   }
 }
 
